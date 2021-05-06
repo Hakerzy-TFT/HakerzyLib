910822307151c5b4d99dc2dc2e8facece400930c
ReformedMrozu <p.mroz.zywiol@gmail.com>
2021-05-06 16:31:19 +0200

    initialized library

---
+++ Core/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HakerzyLib.core
+{
+    public class Message
+    {
+        public static string ToJson(string param)
+        {
+            return "{ \"message\" : \"" + param + "\"}";
+        }
+
+        public static string ToJson(string param, bool isArray)
+        {
+            return isArray ? "{ \"message\" :" + param : "{ \"message\" : \"" + param + "\"}";
+        }
+
+        public static string ToJson(string param, string valueName)
+        {
+            return "{ \"" + valueName + "\" : \"" + param + "\"}";
+        }
+    }
+}
---
+++ HLMain.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace HakerzyLib
+{
+    public class HLMain
+    {
+        
+    }
+}
