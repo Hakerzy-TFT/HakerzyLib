3efb5b833c64dec31963724e3ffb00f82c060db2
ReformedMrozu <p.mroz.zywiol@gmail.com>
2021-06-13 14:10:32 +0200

    added method for validating properties of objects

---
+++ Core/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HakerzyLib.Core
+{
+    public static class Utils
+    {
+        public static bool IsAnyNullOrEmpty(object obj)
+        {
+            if (Object.ReferenceEquals(obj, null))
+                return true;
+
+            return obj.GetType().GetProperties()
+                .Any(x => IsNullOrEmpty(x.GetValue(obj)));
+        }
+
+        private static bool IsNullOrEmpty(object value)
+        {
+            if (Object.ReferenceEquals(value, null))
+                return true;
+
+            var type = value.GetType();
+            return type.IsValueType
+                && Object.Equals(value, Activator.CreateInstance(type));
+
+        }
+    }
+}
